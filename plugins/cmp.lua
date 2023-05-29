e6f234da46429d7e01792ffa70647ca2642e36aa
Stanisław Jakubowski <sjakubowski3554@gmail.com>
2023-05-29 22:14:35 +0200

    Disable snippets and text suggestions

--- plugins/cmp.lua
+++ plugins/cmp.lua
@@ -6,6 +6,15 @@ return {
     opts.completion = {
       completeopt = "menu,menuone,noinsert",
     }
+    opts.sources = {
+      {
+        name = "nvim_lsp",
+        entry_filter = function(entry)
+          local kind = cmp.lsp.CompletionItemKind
+          return kind.Text ~= entry:get_kind() and kind.Snippet ~= entry:get_kind()
+        end,
+      },
+    }
     return opts
   end,
 }
