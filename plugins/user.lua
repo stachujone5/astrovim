8042b0dcb622f6faea7e7f2cfce03c5140a06c62
Stanisław Jakubowski <sjakubowski3554@gmail.com>
2023-05-30 20:42:34 +0200

    Disable all unused plugins

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,6 +1,16 @@
 return {
   -- Disable unused plugins
-  { "lewis6991/gitsigns.nvim", enabled = false },
-  { "rebelot/heirline.nvim", enabled = false },
-  { "nvim-autopairs", enabled = false },
+  { "lewis6991/gitsigns.nvim",      enabled = false },
+  { "rebelot/heirline.nvim",        enabled = false },
+  { "stevearc/aerial.nvim",         enabled = false },
+  { "goolord/alpha-nvim",           enabled = false },
+  { "AstroNvim/astrotheme",         enabled = false },
+  { "max397574/better-escape.nvim", enabled = false },
+  { "stevearc/dressing.nvim",       enabled = false },
+  { "mfussenegger/nvim-dap",        enabled = false },
+  { "jay-babu/mason-nvim-dap.nvim", enabled = false },
+  { "rcarriga/nvim-dap-ui",         enabled = false },
+  { "kevinhwang91/nvim-ufo",        enabled = false },
+  { "s1n7ax/nvim-window-picker",    enabled = false },
+  { "akinsho/toggleterm.nvim",      enabled = false },
 }
